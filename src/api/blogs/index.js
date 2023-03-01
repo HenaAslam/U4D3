3fa9a6ee36b8030768de0a2f98bd17b038e78da4
ajeer333 <ajeer333@gmail.com>
2023-03-01 15:15:16 +0100

    commit

--- src/api/blogs/index.js
+++ src/api/blogs/index.js
@@ -1,5 +1,79 @@
 import Express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import uniqid from "uniqid";
 
 const blogsRouter = Express.Router();
+const blogsJSONPath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "blogs.json"
+);
+const getBlogs = () => JSON.parse(fs.readFileSync(blogsJSONPath));
+const writeBlogs = (array) =>
+  fs.writeFileSync(blogsJSONPath, JSON.stringify(array));
+
+blogsRouter.get("/", (req, res, next) => {
+  try {
+    const blogsArray = getBlogs();
+    res.send(blogsArray);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+blogsRouter.get("/:blogId", (req, res, next) => {
+  try {
+    const blogsArray = getBlogs();
+    const blog = blogsArray.find((blog) => blog.id === req.params.blogId);
+    res.send(blog);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+blogsRouter.post("/", (req, res, next) => {
+  try {
+    const newBlog = {
+      ...req.body,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      id: uniqid(),
+    };
+    const blogsArray = getBlogs();
+    blogsArray.push(newBlog);
+
+    writeBlogs(blogsArray);
+    res.status(201).send({ id: newBlog.id });
+  } catch (error) {
+    console.log(error);
+  }
+});
+blogsRouter.delete("/:blogId", (req, res, next) => {
+  try {
+    const blogsArray = getBlogs();
+    const remainingBlogs = blogsArray.filter(
+      (blog) => blog.id !== req.params.blogId
+    );
+    writeBlogs(remainingBlogs);
+    res.status(204).send();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+blogsRouter.put("/:blogId", (req, res, next) => {
+  try {
+    const blogsArray = getBlogs();
+    const index = blogsArray.findIndex((blog) => blog.id === req.params.blogId);
+    const oldBlog = blogsArray[index];
+    const updatedBlog = { ...oldBlog, ...req.body, updatedAt: new Date() };
+    blogsArray[index] = updatedBlog;
+    writeBlogs(updatedBlog);
+    res.send(updatedBlog);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export default blogsRouter;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import Express from "express";
 import authorRouter from "./api/authors/index.js";
 import listEndpoints from "express-list-endpoints";
+import blogsRouter from "./api/blogs/index.js";
 
 const server = Express();
 server.use(Express.json());
 server.use("/authors", authorRouter);
+server.use("/blogs", blogsRouter);
 
 const port = 3001;
 server.listen(port, () => {
