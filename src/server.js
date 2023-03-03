1b2f1e55f5c7d741aac5fe84f3c70eb0dd5e436a
ajeer333 <ajeer333@gmail.com>
2023-03-03 09:06:16 +0100

    commit

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import authorRouter from "./api/authors/index.js";
 import listEndpoints from "express-list-endpoints";
 import blogsRouter from "./api/blogs/index.js";
 import filesRouter from "./api/files/index.js";
+import { join } from "path";
 import cors from "cors";
 import {
   notFoundHandler,
@@ -12,6 +13,8 @@ import {
 } from "./errorHandlers.js";
 const server = Express();
 const port = 3001;
+const publicFolderPath = join(process.cwd(), "./public");
+server.use(Express.static(publicFolderPath));
 server.use(cors());
 server.use(Express.json());
 server.use("/authors", authorRouter);
