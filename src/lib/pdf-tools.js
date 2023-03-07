5b7fe7fdd0b3fd2b2adc578ccb86550788152f87
ajeer333 <ajeer333@gmail.com>
2023-03-07 18:03:29 +0100

    commit

--- src/lib/pdf-tools.js
+++ src/lib/pdf-tools.js
@@ -1,6 +1,7 @@
 import PdfPrinter from "pdfmake";
 import imageToBase64 from "image-to-base64";
-export const getPDFReadableStream = (blog) => {
+
+export const getPDFReadableStream = async (blog) => {
   const fonts = {
     Courier: {
       normal: "Courier",
@@ -17,14 +18,14 @@ export const getPDFReadableStream = (blog) => {
   };
 
   const printer = new PdfPrinter(fonts);
-  //   const img = imageToBase64(blog.cover);
+  const coverBase64 = await imageToBase64(blog.cover);
   const content = [
     { text: blog.title, style: "header" },
     { text: blog.category, style: "subheader" },
     { text: blog.content, style: "subheader" },
     { text: blog.author.name, style: "subheader" },
-    //    { image: data:image/jpeg;base64,${coverBase64}},
-    // { image: img },
+
+    { image: `data:image/jpeg;base64,${coverBase64}`, width: 150 },
 
     "\n\n",
   ];
